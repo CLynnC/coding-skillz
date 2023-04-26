431b87657464bd54118d138cff8cc5e20ee9e2f7
Crystal Lynn C <crystal.lynnmiller@mail.com>
2023-04-26 00:19:16 -0500

    added the necessary files after coding the changes

---
+++ sendEmail.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("crystal.lynncollins@mail.com", "You Might Have Work!! Here is the subject line",
+
+
+$_POST["fullName","message"]. "From: "+$_POST["email"];
+
+
+}
+
+
+?>
